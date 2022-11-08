021d4bfbe6def9e5c317b3f75ac403594a4e1ffc
samilieberman <samilieberman1@gmail.com>
2022-11-07 20:54:51 -0500

    Changed text in App.js to fix merge conflict

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={gatorLogo} className="App-logo" alt="logo" />
         <p> Hello World! </p>
-        <p> My name is Ana and this is my project </p>
+        <p> My name is Ana and this our team project </p>
       </header>
     </div>
   );
